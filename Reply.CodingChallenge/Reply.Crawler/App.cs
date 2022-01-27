089a3f26d0a7527920bb661aa0ab2e46ed5da8da
nils-jensen <nilsjensendev@gmail.com>
2022-01-27 13:23:47 +0100

    .gitignore and basic solution setup

---
+++ Reply.CodingChallenge/Reply.Crawler/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reply.Crawler
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
